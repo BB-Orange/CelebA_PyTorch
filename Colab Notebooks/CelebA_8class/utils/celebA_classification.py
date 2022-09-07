cbead5b2faf8a0a731e7fba826ba167868be9606
BB-Orange <66671896+BB-Orange@users.noreply.github.com>
2022-09-07 15:58:39 +0900

    AddFolder

---
+++ Colab Notebooks/CelebA_8class/utils/celebA_classification.py
@@ -0,0 +1,159 @@
+import glob
+import os
+import os.path as osp
+import torch.utils.data as data
+from torchvision import models, transforms
+from PIL import Image
+
+
+class ImageTransform():
+    """
+    画像の前処理クラス。訓練時、検証時で異なる動作をする。
+    画像のサイズをリサイズし、色を標準化する。
+    訓練時はRandomResizedCropとRandomHorizontalFlipでデータオーギュメンテーションする。
+
+
+    Attributes
+    ----------
+    resize : int
+        リサイズ先の画像の大きさ。
+    mean : (R, G, B)
+        各色チャネルの平均値。
+    std : (R, G, B)
+        各色チャネルの標準偏差。
+    """
+
+    def __init__(self, resize, mean, std):
+        self.data_transform = {
+            'train': transforms.Compose([
+                transforms.RandomResizedCrop(
+                    resize, scale=(0.5, 1.0)),  # データオーギュメンテーション
+                transforms.RandomHorizontalFlip(),  # データオーギュメンテーション
+                transforms.ToTensor(),  # テンソルに変換
+                transforms.Normalize(mean, std)  # 標準化
+            ]),
+            'val': transforms.Compose([
+                transforms.Resize(resize),  # リサイズ
+                transforms.CenterCrop(resize),  # 画像中央をresize×resizeで切り取り
+                transforms.ToTensor(),  # テンソルに変換
+                transforms.Normalize(mean, std)  # 標準化
+            ])
+        }
+
+    def __call__(self, img, phase='train'):
+        """
+        Parameters
+        ----------
+        phase : 'train' or 'val'
+            前処理のモードを指定。
+        """
+        return self.data_transform[phase](img)
+
+
+def make_datapath_list(datapath, phase="train"):
+    """
+    データのパスを格納したリストを作成する。
+
+    Parameters
+    ----------
+    phase : 'train' or 'val'
+        訓練データか検証データかを指定する
+
+    Returns
+    -------
+    path_list : list
+        データへのパスを格納したリスト
+    """
+
+    rootpath = datapath
+    target_path = osp.join(rootpath+phase+'/**/*.png')
+    print(target_path)
+
+    path_list = []  # ここに格納する
+
+    # globを利用してサブディレクトリまでファイルパスを取得する
+    for path in glob.glob(target_path):
+        path_list.append(path)
+
+    return path_list
+
+def make_label_list(json_path):
+    """
+    データのパスからラベルリストを作成する。
+
+    Parameters
+    ----------
+    phase : 'train' or 'val'
+        訓練データか検証データかを指定する
+
+    Returns
+    -------
+    path_list : list
+        データへのパスを格納したリスト
+    """
+    import json
+
+    # jsonファイルの読込み
+    ILSVRC_class_index = json.load(open(json_path, 'r'))
+
+    # ラベル用リストの作成
+    label_list = []
+    for index in ILSVRC_class_index:
+      label_list.append(ILSVRC_class_index[str(index)][1])
+
+    return label_list
+
+# 
+def PathToLabel(label, label_list):
+  i = 0
+  for folder in label_list:
+    if folder == label:
+      return i
+    i += 1
+
+  return -1
+
+class CelebADataset(data.Dataset):
+    """
+    セレブの画像のDatasetクラス。PyTorchのDatasetクラスを継承。
+
+    Attributes
+    ----------
+    file_list : リスト
+        画像のパスを格納したリスト
+    transform : object
+        前処理クラスのインスタンス
+    phase : 'train' or 'test'
+        学習か訓練かを設定する。
+    """
+
+    def __init__(self, file_list, label_list, transform=None, phase='train'):
+        self.file_list = file_list  # ファイルパスのリスト
+        self.transform = transform  # 前処理クラスのインスタンス
+        self.phase = phase  # train or valの指定
+        self.label_list = label_list
+
+    def __len__(self):
+        '''画像の枚数を返す'''
+        return len(self.file_list)
+
+    def __getitem__(self, index):
+        '''
+        前処理をした画像のTensor形式のデータとラベルを取得
+        '''
+
+        # index番目の画像をロード
+        img_path = self.file_list[index]
+        img = Image.open(img_path)  # [高さ][幅][色RGB]
+
+        # 画像の前処理を実施
+        img_transformed = self.transform(
+            img, self.phase)  # torch.Size([3, 224, 224])
+
+        # 画像のラベルをファイル名から抜き出す
+        label = osp.basename(osp.dirname(img_path))
+
+        # ラベルを数値に変更する
+        label = PathToLabel(label, self.label_list)
+
+        return img_transformed, label
